632292b2a3659f49de7219fdcda18bd03608578c
well <Wellanjo900@gmail.com>
2026-05-31 16:47:33 -0300

    Creating Players and Enemies Moving players and Enemies

--- code/Const.py
+++ code/Const.py
@@ -1,4 +1,6 @@
 #C
+import pygame
+
 COLOR_ORANGE = (255, 128, 0)
 COLOR_WHITE = (255, 255, 255)
 COLOR_YELLOW = (255,255,0)
@@ -10,6 +12,8 @@ MENU_OPTION = ('NEW GAME 1P',
                'SCORE',
                'EXIT')
 #E
+EVENT_ENEMY = pygame.USEREVENT
+
 ENTITY_SPEED = {
     'Level1Bg0': 0,
     'Level1Bg1': 1,
@@ -18,7 +22,30 @@ ENTITY_SPEED = {
     'Level1Bg4': 4,
     'Level1Bg5': 5,
     'Level1Bg6': 6,
+    'Player1': 3,
+    'Player2': 3,
+    'Enemy1': 2,
+    'Enemy2': 1,
+
 }
+
+# P
+
+PLAYER_KEY_UP = {'Player1' : pygame.K_UP,
+                 'Player2' : pygame.K_w}
+PLAYER_KEY_DOWN = {'Player1' : pygame.K_DOWN,
+                 'Player2' : pygame.K_s}
+PLAYER_KEY_LEFT = {'Player1' : pygame.K_LEFT,
+                 'Player2' : pygame.K_a}
+PLAYER_KEY_RIGHT = {'Player1' : pygame.K_RIGHT,
+                 'Player2' : pygame.K_d}
+PLAYER_KEY_SHOOT = {'Player1' : pygame.K_RCTRL,
+                 'Player2' : pygame.K_LCTRL}
+
+#S
+SPAWN_TIME = 4000
+
+
 
 # W
 WIN_WIDTH = 576
---
+++ code/Enemy.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from code.Const import ENTITY_SPEED, WIN_WIDTH
+from code.Entity import Entity
+
+
+class Enemy(Entity):
+
+    def __init__(self, name: str, position: tuple):
+        super().__init__(name, position)
+
+
+    def move(self, ):
+       self.rect.centerx -= ENTITY_SPEED[self.name]
+       if self.rect.right <= 0:
+           self.rect.left = WIN_WIDTH
