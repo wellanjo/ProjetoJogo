6caa4fc2b0bcfc81e932b9d7f49048932db28c5b
well <Wellanjo900@gmail.com>
2026-05-31 22:40:50 -0300

    Creating Mediator Patter Enemies destruction

--- code/Const.py
+++ code/Const.py
@@ -29,6 +29,21 @@ ENTITY_SPEED = {
 
 }
 
+ENTITY_HEALTH = {
+    'Level1Bg0': 999,
+    'Level1Bg1': 999,
+    'Level1Bg2': 999,
+    'Level1Bg3': 999,
+    'Level1Bg4': 999,
+    'Level1Bg5': 999,
+    'Level1Bg6': 999,
+    'Player1': 300,
+    'Player2': 300,
+    'Enemy1': 50,
+    'Enemy2': 60,
+
+}
+
 # P
 
 PLAYER_KEY_UP = {'Player1' : pygame.K_UP,
--- code/Enemy.py
+++ code/Enemy.py
@@ -12,5 +12,4 @@ class Enemy(Entity):
 
     def move(self, ):
        self.rect.centerx -= ENTITY_SPEED[self.name]
-       if self.rect.right <= 0:
-           self.rect.left = WIN_WIDTH
+
