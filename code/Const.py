5c02d12879a019ac190c5e4278551ff175022adb
well <Wellanjo900@gmail.com>
2026-06-01 22:10:42 -0300

    Creating player shot Creating Enemy shot

--- code/Const.py
+++ code/Const.py
@@ -12,7 +12,7 @@ MENU_OPTION = ('NEW GAME 1P',
                'SCORE',
                'EXIT')
 #E
-EVENT_ENEMY = pygame.USEREVENT
+EVENT_ENEMY = pygame.USEREVENT + 1
 
 ENTITY_SPEED = {
     'Level1Bg0': 0,
@@ -23,9 +23,13 @@ ENTITY_SPEED = {
     'Level1Bg5': 5,
     'Level1Bg6': 6,
     'Player1': 3,
+    'Player1Shot': 2,
     'Player2': 3,
-    'Enemy1': 2,
+    'Player2Shot': 3,
+    'Enemy1': 1,
+    'Enemy1Shot': 5,
     'Enemy2': 1,
+    'Enemy2Shot': 2,
 
 }
 
@@ -38,10 +42,20 @@ ENTITY_HEALTH = {
     'Level1Bg5': 999,
     'Level1Bg6': 999,
     'Player1': 300,
+    'Player1Shot': 1,
     'Player2': 300,
+    'Player2Shot': 1,
     'Enemy1': 50,
+    'Enemy1Shot': 1,
     'Enemy2': 60,
+    'Enemy2Shot': 1,
 
+}
+ENTITY_SHOT_DELAY = {
+    'Player1': 20,
+    'Player2': 15,
+    'Enemy1': 100,
+    'Enemy2': 200,
 }
 
 # P
--- code/EntityMediator.py
+++ code/EntityMediator.py
@@ -1,5 +1,9 @@
+
+from code.Const import WIN_WIDTH
 from code.Enemy import Enemy
+from code.EnemyShot import EnemyShot
 from code.Entity import Entity
+from code.PlayerShot import PlayerShot
 
 
 class EntityMediator:
@@ -8,6 +12,12 @@ class EntityMediator:
     def __verify_collision_window(ent: Entity):
         if isinstance(ent, Enemy):
             if ent.rect.right < 0:
+                ent.health = 0
+        if isinstance(ent,PlayerShot):
+            if ent.rect.right >= WIN_WIDTH:
+                ent.health = 0
+        if isinstance(ent, EnemyShot):
+            if ent.rect.right <=0:
                 ent.health = 0
         pass
 
