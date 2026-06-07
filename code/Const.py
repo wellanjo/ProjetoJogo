df9a5b669a6b5e12c7778dafb4874c9d424edf06
well <Wellanjo900@gmail.com>
2026-06-06 21:09:36 -0300

    SQLite 3 implementation for Score using proxy desing pattern

--- code/Const.py
+++ code/Const.py
@@ -1,5 +1,6 @@
-#C
+# C
 import pygame
+from pygame.examples.grid import WINDOW_WIDTH
 
 C_ORANGE = (255, 128, 0)
 C_WHITE = (255, 255, 255)
@@ -13,7 +14,7 @@ MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 1P 2P - COOPERATIVE',
                'SCORE',
                'EXIT')
-#E
+# E
 EVENT_ENEMY = pygame.USEREVENT + 1
 EVENT_TIMEOUT = pygame.USEREVENT + 2
 
@@ -120,24 +121,40 @@ ENTITY_SHOT_DELAY = {
 
 # P
 
-PLAYER_KEY_UP = {'Player1' : pygame.K_UP,
-                 'Player2' : pygame.K_w}
-PLAYER_KEY_DOWN = {'Player1' : pygame.K_DOWN,
-                 'Player2' : pygame.K_s}
-PLAYER_KEY_LEFT = {'Player1' : pygame.K_LEFT,
-                 'Player2' : pygame.K_a}
-PLAYER_KEY_RIGHT = {'Player1' : pygame.K_RIGHT,
-                 'Player2' : pygame.K_d}
-PLAYER_KEY_SHOOT = {'Player1' : pygame.K_RCTRL,
-                 'Player2' : pygame.K_LCTRL}
+PLAYER_KEY_UP = {'Player1': pygame.K_UP,
+                 'Player2': pygame.K_w}
+PLAYER_KEY_DOWN = {'Player1': pygame.K_DOWN,
+                   'Player2': pygame.K_s}
+PLAYER_KEY_LEFT = {'Player1': pygame.K_LEFT,
+                   'Player2': pygame.K_a}
+PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT,
+                    'Player2': pygame.K_d}
+PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
+                    'Player2': pygame.K_LCTRL}
 
-#S
+# S
 SPAWN_TIME = 4000
 
-#T
-TIMEOUT_STEP = 100 # 100ms
-TIMEOUT_LEVEL = 30000 #20s
+
+# T
+TIMEOUT_STEP = 100  # 100ms
+TIMEOUT_LEVEL = 20000  # 20s
 
 # W
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
+
+#S
+SCORE_POS = {'Title': (WIN_WIDTH / 2, 50),
+             'EnterName': (WIN_WIDTH / 2, 80),
+             'Label': (WIN_WIDTH / 2, 90),
+             'Name': (WIN_WIDTH / 2, 110),
+             0: (WIN_WIDTH / 2, 110),
+             1: (WIN_WIDTH / 2, 130),
+             2: (WIN_WIDTH / 2, 150),
+             3: (WIN_WIDTH / 2, 170),
+             4: (WIN_WIDTH / 2, 190),
+             5: (WIN_WIDTH / 2, 210),
+             6: (WIN_WIDTH / 2, 230),
+
+}
--- code/Game.py
+++ code/Game.py
@@ -5,6 +5,7 @@ import pygame
 from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
 from code.Menu import Menu
 from code.Level import Level
+from code.Score import Score
 
 
 class Game:
@@ -13,10 +14,8 @@ class Game:
         self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
-
-
-
         while True:
+            score = Score(self.window)
             menu = Menu(self.window)
             menu_return = menu.run()
 
@@ -27,6 +26,12 @@ class Game:
                 if level_return:
                     level = Level(self.window, 'Level2', menu_return, player_score)
                     level_return = level.run(player_score)
+                    if level_return:
+                        score.save(menu_return, player_score)
+
+
+            elif menu_return == MENU_OPTION[3]:
+                score.show()
             elif menu_return == MENU_OPTION[4]:
                 pygame.quit()
                 quit()
